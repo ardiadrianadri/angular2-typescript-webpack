4913ee642a0c1b2e3c68e3df69cc2c710b0dab25
ardiadrianadri <ardiadrianadri@gmail.com>
2016-05-02 12:32:58 +0200

    Services finish

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,29 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Heroe} from "./heroe.entity";
-import {HEROES} from "./mock-heroes";
 import {HeroeDetailComponent} from "./heroe-detail.component";
+import {HeroeService} from "./heroe.service";
 require('./style/style.css');
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
-    directives: [HeroeDetailComponent]
+    directives: [HeroeDetailComponent],
+    providers: [HeroeService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
     title= 'Tour of Heroes';
     selectedHeroe: Heroe;
-    heroes:Heroe[] = HEROES;
+    heroes:Heroe[];
+
+    constructor(
+        private _heroeService:HeroeService
+    ){}
+
+    ngOnInit(){
+        this._heroeService.getHeroes().then(heroes=>this.heroes = heroes, error=>console.error(JSON.stringify(error)));
+    }
     
     onSelect(heroe: Heroe){
         this.selectedHeroe = heroe;
